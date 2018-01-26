5aca76aecc150f75e4c45fd983bbfe0f6e795f04
Lewis Nyoike <nyoikelewis@gmail.com>
2018-01-26 11:47:15 +0300

    Allowed the dormitory_id params

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -69,6 +69,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:name, :admission_number)
+      params.require(:student).permit(:name, :admission_number, :dormitory_id)
     end
 end
