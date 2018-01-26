0823cc074205224f364cae216a2cf0c8c588380b
Lewis Nyoike <nyoikelewis@gmail.com>
2018-01-26 13:39:24 +0300

    MIgrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126085148) do
+ActiveRecord::Schema.define(version: 20180126102004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20180126085148) do
     t.datetime "due_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "subject_id"
+    t.bigint "level_id"
+    t.index ["level_id"], name: "index_assignments_on_level_id"
+    t.index ["subject_id"], name: "index_assignments_on_subject_id"
   end
 
   create_table "dormitories", force: :cascade do |t|
@@ -109,6 +113,8 @@ ActiveRecord::Schema.define(version: 20180126085148) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "assignments", "levels"
+  add_foreign_key "assignments", "subjects"
   add_foreign_key "exams", "levels"
   add_foreign_key "exams", "subjects"
   add_foreign_key "students", "dormitories"
