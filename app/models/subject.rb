867daf5e316dc021a2f654976453c708ac08594b
Lewis Nyoike <nyoikelewis@gmail.com>
2018-01-29 00:52:17 +0300

    Relationship with students through the joint table

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,7 +1,9 @@
 class Subject < ApplicationRecord
   has_many :exams
   has_many :assignments
-  
+  has_many :student_subjects
+  has_many :students, through: :student_subjects
+
   private
 
   def name_with_state
